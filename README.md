baac141a6c8db91da0cb93ace413ae09c188acaf
Hee Kim <hee.kim@roche.com>
2023-01-24 13:17:02 -0800

    initialize

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # sbxbam2pe
+
+## Build Requirement in SC1
+
+- module load jdk/11.0.2
+- module load maven/3.5.0
+
+
+## Build
+
+1. git clone git@ghe-rss.roche.com:kimh89/sbxbam2pe.git
+2. got sbxbam2pe
+3. mvn clean package
+
+- bam2pe.jar will be located at ./target dir.
+- bam2pe.jar is executalbe and can be deployed without dependencies.
+
+## Run
+
+### Usage
+```
+java -jar ./target/bam2pe.jar -h
+Unknown option: '-h'
+Usage: <main class> [-i=<inputBam>] [-l=<maxReadLength>] [-o=<outputDir>]
+                    [-p=<outputPrefix>]
+  -i, --input-bam=<inputBam>
+         input BAM file path
+  -l, --max-read-length=<maxReadLength>
+         maximum read length of R1 and R2
+  -o, --output-dir=<outputDir>
+         output directory
+  -p, --prefix=<outputPrefix>
+         prefix of the output R1 and R2 files
+```
+
+### Examples
+
+1. output current directory
+
+Since the default prefix of outputs is "PE_", PE_R1.fastq and  PE_R2.fastq will be generated in the current directory.
+
+```
+java -jar ./target/bam2pe.jar -i /sc1/groups/onco/Analysis/bioinfo_analyses/kimh89/SBX/bfx-ngs-AVC-225/pipeline/examples/SC1/output_demux2samples_10k_params/analysis/cfDNA7/mergeBam/cfDNA7.merged.bam
+```
+
+2. output a different directory with different prefix.
+
+From the following command, sorted_R1.fastq and sorted_R2.fastq will be generated in ./target dir.
+
+```
+java -jar ./target/bam2pe.jar -p sorted_ -o ./target -i /sc1/groups/onco/Analysis/bioinfo_analyses/kimh89/SBX/bfx-ngs-AVC-225/pipeline/examples/SC1/output_demux2samples_10k_params/analysis/cfDNA7/mergeBam/cfDNA7.merged.bam
+```
+
+
+
---
+++ src/main/java/com/roche/bfx/sbx/gist/Main.java
@@ -0,0 +1,149 @@
+package com.roche.bfx.sbx.gist;
+
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import picocli.CommandLine;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Path;
+import java.util.Iterator;
+
+
+public class Main implements Runnable{
+    @CommandLine.Option(names = {"-i", "--input-bam"}, description = "input BAM file path")
+    private File inputBam;
+
+    @CommandLine.Option(names = {"-o", "--output-dir"}, description = "output directory", defaultValue = ".")
+    private Path outputDir;
+
+    @CommandLine.Option(names = {"-p", "--prefix"}, description = "prefix of the output R1 and R2 files", defaultValue = "PE_")
+    private String outputPrefix;
+
+    @CommandLine.Option(names = {"-l", "--max-read-length"}, description = "maximum read length of R1 and R2", defaultValue = "150")
+    private int maxReadLength;
+
+
+    public static void main(String[] args) {
+        final int exitCode = new CommandLine(new Main()).execute(args);
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    public void run() {
+        final File file1 = new File(outputDir + "/" + outputPrefix+"R1.fastq");
+        final File file2 = new File(outputDir + "/" + outputPrefix+"R2.fastq");
+
+        try (SamReader reader = SamReaderFactory.makeDefault().open(inputBam)) {
+            BufferedWriter writer1 = new BufferedWriter(new FileWriter(file1));
+            BufferedWriter writer2 = new BufferedWriter(new FileWriter(file2));
+
+            Iterator<SAMRecord> iter = reader.iterator();
+            while (iter.hasNext()) {
+                SAMRecord record = iter.next();
+                String r1 = convertSAMRecordToR1(record);
+                String r2 = convertSAMRecordToR2(record);
+                writer1.write(r1);
+                writer2.write(r2);
+            }
+
+        } catch (final Exception e) {
+            System.out.println(e.getMessage());
+            if(file1.exists()) file1.delete();
+            if(file2.exists()) file2.delete();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * @param record
+     * @return fastq string
+     */
+    private String convertSAMRecordToR1(SAMRecord record){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("@"+record.getReadName()).append(" 1:N:0").append("\n");
+        if(record.getReadString().length() < maxReadLength) {
+            sb.append(record.getReadString()).append("\n");
+            sb.append("+").append("\n");
+            sb.append(record.getBaseQualityString()).append("\n");
+        }else{
+            sb.append(record.getReadString().substring(0,maxReadLength)).append("\n");
+            sb.append("+").append("\n");
+            sb.append(record.getBaseQualityString().substring(0, maxReadLength)).append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * @param record
+     * @return fastq string
+     */
+    private String convertSAMRecordToR2(SAMRecord record){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("@"+record.getReadName()).append(" 2:N:0").append("\n");
+        if(record.getReadString().length() < maxReadLength) {
+            sb.append(reverseComp(record.getReadString())).append("\n");
+            sb.append("+").append("\n");
+            sb.append(reverse(record.getBaseQualityString())).append("\n");
+        }else{
+            sb.append(reverseComp(record.getReadString().substring(record.getReadLength() - maxReadLength))).append("\n");
+            sb.append("+").append("\n");
+            sb.append(reverse(record.getBaseQualityString().substring(record.getReadLength()-maxReadLength))).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private String reverseComp(String input){
+        return complement(reverse(input));
+    }
+
+    private String reverse(String input){
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+
+        return sb.reverse().toString();
+    }
+
+    private String complement(String input){
+        StringBuilder sb = new StringBuilder();
+        char[] chars = input.toCharArray();
+        for(char c : chars){
+            switch(c) {
+                case 'A':
+                    sb.append('T');
+                    break;
+                case 'T':
+                    sb.append('A');
+                    break;
+                case 'G':
+                    sb.append('C');
+                    break;
+                case 'C':
+                    sb.append('G');
+                    break;
+                case 'N':
+                    sb.append('N');
+                default:
+                    break;
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
+
+
